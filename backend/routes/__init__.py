2015dafcea4d5c9eeb098c69f4119bce34437b52
Karan Parwani <karanparwani006@gmail.com>
2023-04-29 17:38:35 +0400

    changed database connection

--- backend/routes/__init__.py
+++ backend/routes/__init__.py
@@ -8,8 +8,23 @@ def get_db_conn():
        :returns: Returns connection object or exception it it fails. 
     '''
     try:
-        database_path = os.path.join(os.getcwd(), 'database\Furniture.db') 
-        conn = sqlite3.connect(database_path, check_same_thread=False)
-        return conn
+        database_path = os.path.join(os.getcwd(), 'database/Furniture.db') 
+        c = sqlite3.connect(database_path)
+        connection = c.cursor()
+        table_name = 'furniture'
+        connection.execute(f"SELECT name FROM sqlite_master WHERE type='table' AND name='{table_name}'")
+        result = connection.fetchone()
+        
+        if result is None:
+            connection.execute('''CREATE TABLE IF NOT EXISTS furniture
+                                (id INTEGER PRIMARY KEY,
+                                name TEXT,
+                                description TEXT,
+                                price REAL,
+                                comment text);''')
+            return c
+        else:
+            return c
+    
     except Exception as e:
         print(e)
